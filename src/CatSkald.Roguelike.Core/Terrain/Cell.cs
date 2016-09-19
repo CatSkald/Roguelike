4a8a9b6382b0e435a600364389e7c724fd19eb3a
Cat Skald <CatSkald@users.noreply.github.com>
2016-09-20 00:17:10 +0200

    Added IEquatable for Cell.

--- src/CatSkald.Roguelike.Core/Terrain/Cell.cs
+++ src/CatSkald.Roguelike.Core/Terrain/Cell.cs
@@ -1,11 +1,34 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using CatSkald.Roguelike.Core.Objects;
 
 namespace CatSkald.Roguelike.Core.Terrain
 {
-    public sealed class Cell : ICell
+    public sealed class Cell : ICell, IEquatable<Cell>
     {
         public Point Location { get; set; }
         public XType Type { get; set; }
+
+        #region IEquatable
+        public bool Equals(Cell other)
+        {
+            return Location == other.Location
+                && Type == other.Type;
+        }
+
+        public override int GetHashCode()
+        {
+            var result = Type.GetHashCode();
+            result = (result * 397) ^ Location.GetHashCode();
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType())
+                return false;
+            return this.Equals((Cell)obj);
+        }
+        #endregion
     }
 }
