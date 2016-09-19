e3820c0e7e705362a9bbae0761c0b64fc3414fc6
Cat Skald <CatSkald@users.noreply.github.com>
2016-09-20 00:19:00 +0200

    Added stairs creation.

--- src/CatSkald.Roguelike.GameProcessor/Initialization/DungeonPopulator.cs
+++ src/CatSkald.Roguelike.GameProcessor/Initialization/DungeonPopulator.cs
@@ -1,4 +1,7 @@
-﻿using CatSkald.Roguelike.Core.Terrain;
+﻿using System.Linq;
+using CatSkald.Roguelike.Core.Objects;
+using CatSkald.Roguelike.Core.Terrain;
+using CatSkald.Tools;
 
 namespace CatSkald.Roguelike.GameProcessor.Initialization
 {
@@ -7,6 +10,21 @@ namespace CatSkald.Roguelike.GameProcessor.Initialization
         public void Fill(IDungeon dungeon)
         {
             ////TODO create all the stuff and place into dungeon
+
+            CreateStairs(dungeon);
+        }
+
+        private static void CreateStairs(IDungeon dungeon)
+        {
+            var emptyCells = dungeon.Where(cell => cell.Type == XType.Empty).ToList();
+            var count = emptyCells.Count;
+            var upstairsPosition = StaticRandom.Next(emptyCells.Count);
+            var downstairsPosition = StaticRandom.NextNotEqualToOld(
+                0, emptyCells.Count, upstairsPosition);
+            var upstairsCell = emptyCells[upstairsPosition];
+            var downstairsCell = emptyCells[downstairsPosition];
+            dungeon[upstairsCell].Type = XType.StairsUp;
+            dungeon[downstairsCell].Type = XType.StairsDown;
         }
     }
 }
