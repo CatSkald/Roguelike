b6b960db8597d5c332fda985bd69fe9564b68b5e
Cat Skald <CatSkald@users.noreply.github.com>
2016-09-20 00:34:46 +0200

    Added DungeonPopulator tests.

---
+++ test/CatSkald.Rogualike.Test.GameProcessor.UnitTests/Initialization/DungeonPopulatorTests.cs
@@ -0,0 +1,43 @@
+﻿using CatSkald.Rogualike.Test.GameProcessor.UnitTests.TestHelpers;
+using CatSkald.Roguelike.Core.Objects;
+using CatSkald.Roguelike.Core.Terrain;
+using CatSkald.Roguelike.GameProcessor.Initialization;
+using NUnit.Framework;
+
+namespace CatSkald.Rogualike.Test.GameProcessor.UnitTests.Initialization
+{
+    public class DungeonPopulatorTests
+    {
+        [Test]
+        public void Fill_UpstairsAdded()
+        {
+            var populator = new DungeonPopulator();
+            var dungeon = new FakeDungeon(5, 3);
+            dungeon[2, 2].Type = XType.Empty;
+            dungeon[3, 3].Type = XType.Empty;
+
+            populator.Fill(dungeon);
+
+            Assert.That(dungeon,
+                Has.Exactly(1)
+                .With.Property(nameof(Cell.Type))
+                .EqualTo(XType.StairsUp));
+        }
+
+        [Test]
+        public void Fill_DownstairsAdded()
+        {
+            var populator = new DungeonPopulator();
+            var dungeon = new FakeDungeon(5, 3);
+            dungeon[2, 2].Type = XType.Empty;
+            dungeon[3, 3].Type = XType.Empty;
+
+            populator.Fill(dungeon);
+
+            Assert.That(dungeon,
+                Has.Exactly(1)
+                .With.Property(nameof(Cell.Type))
+                .EqualTo(XType.StairsDown));
+        }
+    }
+}
