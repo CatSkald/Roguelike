41c06c8b14691003ba4388a4265d3901bfed54b7
Cat Skald <CatSkald@users.noreply.github.com>
2016-09-19 19:17:47 +0200

    Added test for converter.

---
+++ test/CatSkald.Roguelike.Test.DungeonGenerator.UnitTests/Services/MapConverterTests.cs
@@ -0,0 +1,25 @@
+﻿using CatSkald.Roguelike.DungeonGenerator.Services;
+using CatSkald.Roguelike.DungeonGenerator.Terrain;
+using NUnit.Framework;
+
+namespace CatSkald.Roguelike.Test.DungeonGenerator.UnitTests.Services
+{
+    public class MapConverterTests
+    {
+        [TestCase(1, 6)]
+        [TestCase(100, 3)]
+        public void ConvertToDungeon_ReturnsDungeon_WithCorrectSize(int w, int h)
+        {
+            var converter = new MapConverter();
+            var map = new Map(w, h);
+            var expectedWidth = w * 2 + 1;
+            var expectedHeight = h * 2 + 1;
+
+            var dungeon = converter.ConvertToDungeon(new Map(w, h));
+
+            Assert.That(dungeon, 
+                Has.Property(nameof(Dungeon.Width)).EqualTo(expectedWidth)
+                .And.Property(nameof(Dungeon.Height)).EqualTo(expectedHeight));
+        } 
+    }
+}
