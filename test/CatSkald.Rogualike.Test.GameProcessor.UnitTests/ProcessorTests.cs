dd15648cb738e3bbc59e429a51aaf7480ba8d864
Cat Skald <CatSkald@users.noreply.github.com>
2016-09-19 22:05:46 +0200

    Added tests for Processor.

--- test/CatSkald.Rogualike.Test.GameProcessor.UnitTests/ProcessorTests.cs
+++ test/CatSkald.Rogualike.Test.GameProcessor.UnitTests/ProcessorTests.cs
@@ -12,10 +12,25 @@ namespace CatSkald.Rogualike.Test.GameProcessor.UnitTests
         [Test]
         public void Initialize_CorrectDungeonIsSet()
         {
-            var parameters = new DungeonParameters();
             var dungeon = new FakeDungeon();
             var mapBuilder = Substitute.For<IMapBuilder>();
-            mapBuilder.Build(parameters).Returns(dungeon);
+            mapBuilder.Build(Arg.Any<DungeonParameters>())
+                .Returns(dungeon);
+
+            var processor = new Processor(
+                mapBuilder,
+                Substitute.For<IDungeonPopulator>(),
+                Substitute.For<IMapPainter>());
+            processor.Initialize(new DungeonParameters());
+
+            Assert.That(processor.Dungeon, Is.SameAs(dungeon));
+        }
+        
+        [Test]
+        public void Initialize_BuilderIsCalled()
+        {
+            var parameters = new DungeonParameters();
+            var mapBuilder = Substitute.For<IMapBuilder>();
 
             var processor = new Processor(
                 mapBuilder,
@@ -23,7 +38,44 @@ namespace CatSkald.Rogualike.Test.GameProcessor.UnitTests
                 Substitute.For<IMapPainter>());
             processor.Initialize(parameters);
 
-            Assert.That(processor.Dungeon, Is.SameAs(dungeon));
+            mapBuilder.Received(1).Build(parameters);
+        }
+
+        [Test]
+        public void Initialize_PopulatorIsCalled()
+        {
+            var parameters = new DungeonParameters();
+            var dungeon = new FakeDungeon();
+            var mapBuilder = Substitute.For<IMapBuilder>();
+            mapBuilder.Build(parameters).Returns(dungeon);
+            var populator = Substitute.For<IDungeonPopulator>();
+
+            var processor = new Processor(
+                mapBuilder,
+                populator,
+                Substitute.For<IMapPainter>());
+            processor.Initialize(parameters);
+
+            populator.Received(1).Fill(dungeon);
+        }
+
+        [Test]
+        public void Process_PainterIsCalled()
+        {
+            var parameters = new DungeonParameters();
+            var dungeon = new FakeDungeon();
+            var mapBuilder = Substitute.For<IMapBuilder>();
+            mapBuilder.Build(parameters).Returns(dungeon);
+            var painter = Substitute.For<IMapPainter>();
+
+            var processor = new Processor(
+                mapBuilder,
+                Substitute.For<IDungeonPopulator>(),
+                Substitute.For<IMapPainter>());
+            processor.Initialize(parameters);
+            processor.Process();
+
+            painter.Received(1).DrawMap(dungeon);
         }
     }
 }
