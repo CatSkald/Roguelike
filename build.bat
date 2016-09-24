3df644ec59027ee9116b8dfd31ccdd27bf2f7e42
Cat Skald <CatSkald@users.noreply.github.com>
2016-09-24 18:39:48 +0200

    Refactored build scripts.

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cls
-.\tools\NuGet\NuGet.exe Install FAKE -OutputDirectory packages -ExcludeVersion
-.\tools\NuGet\NuGet.exe Install OpenCover -Version 4.6.519 -OutputDirectory packages
-.\tools\NuGet\NuGet.exe Install coveralls.net -Version 0.412.0 -OutputDirectory packages
-
-.\packages\FAKE\tools\Fake.exe build.fsx
-pause
