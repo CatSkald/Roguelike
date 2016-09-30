a2d6557346851ed79be78be24abc0fca612d289f
Cat Skald <CatSkald@users.noreply.github.com>
2016-09-30 23:16:59 +0200

    Added tests for character and its creation.

--- test/CatSkald.Rogualike.Test.GameProcessor.UnitTests/Initialization/DungeonPopulatorTests.cs
+++ test/CatSkald.Rogualike.Test.GameProcessor.UnitTests/Initialization/DungeonPopulatorTests.cs
@@ -1,4 +1,6 @@
-﻿using CatSkald.Rogualike.Test.GameProcessor.UnitTests.TestHelpers;
+﻿using System.Drawing;
+using System.Linq;
+using CatSkald.Rogualike.Test.GameProcessor.UnitTests.TestHelpers;
 using CatSkald.Roguelike.Core.Cells;
 using CatSkald.Roguelike.GameProcessor.Initialization;
 using NUnit.Framework;
@@ -12,15 +14,16 @@ namespace CatSkald.Rogualike.Test.GameProcessor.UnitTests.Initialization
         {
             var populator = new DungeonPopulator();
             var dungeon = new FakeDungeon(5, 7);
-            dungeon[2, 2].Type = XType.Empty;
-            dungeon[3, 3].Type = XType.Empty;
+            var points = new[] { new Point(2, 2), new Point(3, 3) };
+            foreach (var p in points)
+                dungeon[p].Type = XType.Empty;
 
             populator.Fill(dungeon);
 
             Assert.That(dungeon,
-                Has.Exactly(1)
-                .With.Property(nameof(Cell.Type))
-                .EqualTo(XType.StairsUp));
+                Has.Exactly(1).Matches<Cell>(cell =>
+                    cell.Type == XType.StairsUp
+                    && points.Contains(cell.Location)));
         }
 
         [Test]
@@ -28,15 +31,37 @@ namespace CatSkald.Rogualike.Test.GameProcessor.UnitTests.Initialization
         {
             var populator = new DungeonPopulator();
             var dungeon = new FakeDungeon(5, 7);
-            dungeon[2, 2].Type = XType.Empty;
-            dungeon[3, 3].Type = XType.Empty;
+            var points = new[] { new Point(2, 2), new Point(3, 3) };
+            foreach (var p in points)
+                dungeon[p].Type = XType.Empty;
 
             populator.Fill(dungeon);
 
             Assert.That(dungeon,
-                Has.Exactly(1)
-                .With.Property(nameof(Cell.Type))
-                .EqualTo(XType.StairsDown));
+                Has.Exactly(1).Matches<Cell>(cell => 
+                    cell.Type == XType.StairsDown 
+                    && points.Contains(cell.Location)));
+        }
+
+        [Test]
+        public void Fill_CharacterAdded()
+        {
+            var populator = new DungeonPopulator();
+            var dungeon = new FakeDungeon(5, 7);
+            var points = new[] { new Point(2, 2), new Point(3, 3) };
+            foreach (var p in points)
+                dungeon[p].Type = XType.Empty;
+
+            populator.Fill(dungeon);
+
+            var expectedLocation = dungeon
+                .Single(cell => cell.Type == XType.StairsUp)
+                .Location;
+
+            Assert.That(dungeon.Character,
+                Is.Not.Null
+                .And.With.Property(nameof(Character.Location))
+                .EqualTo(expectedLocation));
         }
     }
 }
---
+++ test/CatSkald.Roguelike.Test.Core.UnitTests/Cells/CharacterTests.cs
@@ -0,0 +1,15 @@
+﻿using CatSkald.Roguelike.Core.Cells;
+using NUnit.Framework;
+namespace CatSkald.Roguelike.Test.Core.UnitTests.Cells
+{
+    public class CharacterTests
+    {
+        [Test]
+        public void Constructor_SetsCorrectType()
+        {
+            var tile = new Character();
+
+            Assert.That(tile.Type, Is.EqualTo(XType.Character));
+        }
+    }
+}
