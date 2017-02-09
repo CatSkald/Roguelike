1b7001cb9c5fbd2f9af9f87c46a145a7ec80fb37
Cat Skald <CatSkald@users.noreply.github.com>
2017-02-09 14:00:55 +0100

    Another try to fix travis build.

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ $NUGET_PATH install OpenCover -Version 4.6.519 -OutputDirectory packages
 $NUGET_PATH install coveralls.net -Version 0.412.0 -OutputDirectory packages
 packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 else
-mono $NUGET_PATH install FAKE.Core -OutputDirectory packages -ExcludeVersion
+mono $NUGET_PATH install FAKE -OutputDirectory packages -ExcludeVersion
 mono $NUGET_PATH install System.Net.Http -OutputDirectory packages
 mono $NUGET_PATH install Microsoft.Net.Http -OutputDirectory packages
 mono $NUGET_PATH install OpenCover -Version 4.6.519 -OutputDirectory packages
 mono $NUGET_PATH install coveralls.net -Version 0.412.0 -OutputDirectory packages
-mono packages/FAKE.Core/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
