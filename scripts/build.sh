acca2fc3b70210fc103fbae7375e4b27c0b87510
Cat Skald <CatSkald@users.noreply.github.com>
2017-02-09 13:22:03 +0100

    Moved build files to separate directory.

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NUGET_PATH=tools/NuGet/NuGet.exe
+
+if test "$OS" = "Windows_NT"
+then
+$NUGET_PATH install FAKE -OutputDirectory packages -ExcludeVersion
+$NUGET_PATH install OpenCover -Version 4.6.519 -OutputDirectory packages
+$NUGET_PATH install coveralls.net -Version 0.412.0 -OutputDirectory packages
+packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+else
+mono $NUGET_PATH install FAKE -OutputDirectory packages -ExcludeVersion
+mono $NUGET_PATH install System.Net.Http -OutputDirectory packages
+mono $NUGET_PATH install Microsoft.Net.Http -OutputDirectory packages
+mono $NUGET_PATH install OpenCover -Version 4.6.519 -OutputDirectory packages
+mono $NUGET_PATH install coveralls.net -Version 0.412.0 -OutputDirectory packages
+mono tools/FAKE.Core/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+fi
