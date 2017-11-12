670895b2f26dfd5a28eb9466a6b5a2b2bb054166
CatSkald <CatSkald@users.noreply.github.com>
2017-11-12 15:12:36 +0100

    Fix tests.

--- src/CatSkald.Roguelike.DungeonGenerator/Properties/AssemblyInfo.cs
+++ src/CatSkald.Roguelike.DungeonGenerator/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("CatSkald.Roguelike.DungeonGenerator")]
 [assembly: InternalsVisibleTo("CatSkald.Roguelike.Test.DungeonGenerator.UnitTests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
