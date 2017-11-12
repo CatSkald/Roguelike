daf07102bef0d5f39bd5964eed3258db0ac0850b
CatSkald <CatSkald@users.noreply.github.com>
2017-11-12 12:13:05 +0100

    Revert travis build back to mono.

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ $NUGET_PATH install OpenCover -Version 4.6.519 -OutputDirectory packages
 $NUGET_PATH install coveralls.net -Version 0.7.0 -OutputDirectory packages
 packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 else
-$NUGET_PATH install FAKE -OutputDirectory packages -ExcludeVersion
-$NUGET_PATH install System.Net.Http -OutputDirectory packages
-$NUGET_PATH install Microsoft.Net.Http -OutputDirectory packages
-$NUGET_PATH install OpenCover -Version 4.6.519 -OutputDirectory packages
-$NUGET_PATH install coveralls.net -Version 0.7.0 -OutputDirectory packages
-packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+mono $NUGET_PATH install FAKE -OutputDirectory packages -ExcludeVersion
+mono $NUGET_PATH install System.Net.Http -OutputDirectory packages
+mono $NUGET_PATH install Microsoft.Net.Http -OutputDirectory packages
+mono $NUGET_PATH install OpenCover -Version 4.6.519 -OutputDirectory packages
+mono $NUGET_PATH install coveralls.net -Version 0.7.0 -OutputDirectory packages
+mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
