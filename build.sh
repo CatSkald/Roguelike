121bdd34e5b59d30732fb9301fa4a199cacbb327
CatSkald <CatSkald@users.noreply.github.com>
2018-05-26 23:55:09 +0200

    Fix path in build.sh.

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ PAKET_PATH=.paket/paket.exe
 
 if test "$OS" = "Windows_NT"
 then
-  PAKET_PATH install
+  $PAKET_PATH install
   packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 else
-  mono PAKET_PATH install
+  mono $PAKET_PATH install
   mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
